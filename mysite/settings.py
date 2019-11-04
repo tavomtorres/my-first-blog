976fde365507a7b4a71247be049e9c887bc029f8
Gustavo <gmendeztorres5d@gmail.com>
2019-11-04 18:31:42 -0300

    cambios extras, inicio de sesion para usuario y se agrego urls en mysite

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,6 +85,9 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
+LOGIN_REDIRECT_URL = '/'
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
